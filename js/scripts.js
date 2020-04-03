8c24d25becafa07062116d7410c8404cc9f2f90a
nitunwa <nitun.2@gmail.com>
2020-04-03 13:39:05 -0700

    add changes in implementation

--- js/scripts.js
+++ js/scripts.js
@@ -13,30 +13,33 @@ function createRange(number){
 }
 
 function replace(num){
-  var highestDigit = findHightestValDigit(num);
-  var replaceStr ="";
-  if(highestDigit === 3){
-    replaceStr = "Won't you be my neighbor?";
-  }else if(highestDigit === 2){
-    replaceStr = "Boop!";
-  }else{
-    replaceStr = "Beep!";
-  }
- return replaceStr;
-}
-
-function findHightestValDigit(numInput){
-  var str = numInput.toString();
+  var str = num.toString();
   var strArray = str.split('');
-  var maxNum = 0;
-  for(var i=0; i<strArray.length; i++){
-    var val = parseInt(strArray[i]);
-    if(val > maxNum) {
-      maxNum = val;
-    }
+  var replaceStr ="";
+  if(strArray.includes(3)){
+    replaceStr = "Won't you be my neighbor?";
+  }else if(strArray.includes(2)){
+    replaceStr = "Boop!";
+  }else if(strArray.includes(3)){
+    replaceStr = "Beep!";
+  }else{
+    replaceStr = strArray;
   }
-  return maxNum;
-}
+return replaceStr;
+};
+
+// function findHightestValDigit(numInput){
+//   var str = numInput.toString();
+//   var strArray = str.split('');
+//   var maxNum = 0;
+//   for(var i=0; i<strArray.length; i++){
+//     var val = parseInt(strArray[i]);
+//     if(val > maxNum) {
+//       maxNum = val;
+//     }
+//   }
+//   return maxNum;
+// }
 
 $(document).ready(function() {
   $("#intro").submit(function(event){
