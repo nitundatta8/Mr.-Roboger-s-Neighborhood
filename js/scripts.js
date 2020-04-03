1e1d52eb3fbdd1354e343f40c61d09d702c294f8
nitunwa <nitun.2@gmail.com>
2020-04-03 12:54:20 -0700

    add implementation for spec 3 to get highest digit from a number

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,42 @@ function createRange(number){
     var replaceVal = replace(index);
     numArray.push(replaceVal);
   }
+  console.log(numArray)
   return numArray;
 }
 
 function replace(num){
-  return num;
+  var highestDigit = findHightestValDigit(num);
+  var replaceStr ="";
+  if(highestDigit === 3){
+    replaceStr = "Won't you be my neighbor?";
+  }else if(highestDigit === 2){
+    replaceStr = "Boop!";
+  }else{
+    replaceStr = "Beep!";
+  }
+ return replaceStr;
 }
 
+function findHightestValDigit(numInput){
+  var str = numInput.toString();
+  var strArray = str.split('');
+  var maxNum = 0;
+  for(var i=0; i<strArray.length; i++){
+    var val = parseInt(strArray[i]);
+    if(val > maxNum) {
+      maxNum = val;
+    }
+  }
+  return maxNum;
+}
 
 $(document).ready(function() {
-  $("#range").submit(function(event){
+  $("#intro").submit(function(event){
      event.preventDefault();
      var num = $("#demo").val();
      var result = createRange(parseInt(num));
+     $("#output").text(result.join(','));
   });
   
 });
