616ec2e5398e3bb0210bb445a5fb7254e31a23f7
nitunwa <nitun.2@gmail.com>
2020-04-03 14:45:01 -0700

    add changes in replace method

--- js/scripts.js
+++ js/scripts.js
@@ -16,17 +16,24 @@ function replace(num){
   var str = num.toString();
   var strArray = str.split('');
   var replaceStr ="";
-  if(strArray.includes(3)){
-    replaceStr = "Won't you be my neighbor?";
-  }else if(strArray.includes(2)){
+  if(strArray.includes('3')){
+     replaceStr = "Won't you be my neighbor?";
+     return replaceStr;
+  }else if(strArray.includes('2')){
     replaceStr = "Boop!";
-  }else if(strArray.includes(3)){
-    replaceStr = "Beep!";
+    return replaceStr;
+  }else if(strArray.includes('1')){
+  replaceStr = "Beep!";
+  return replaceStr;
   }else{
-    replaceStr = strArray;
+    replaceStr = strArray
   }
 return replaceStr;
-};
+
+}
+
+var result = replace(13);
+result
 
 // function findHightestValDigit(numInput){
 //   var str = numInput.toString();
